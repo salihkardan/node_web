6f74b9321ccca199cd8852169903817d1fa912a8
Salih Kardan <kardan38@gmail.com>
2016-02-26 00:15:23 +0200

    inform user in case of failed login

--- public/js/directives.js
+++ public/js/directives.js
@@ -5,8 +5,6 @@ app.directive('pwCheck', function () {
         require: 'ngModel',
         link: function (scope, elem, attrs, ctrl) {
             var password = "#" + attrs.pwCheck;
-            console.log(password);
-            console.log("jere");
             $(elem).add(password).on('keyup', function() {
                 scope.$apply(function () {
                     ctrl.$setValidity('pwmatch', elem.val() === scope[attrs.pwCheck]);
